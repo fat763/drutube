c82ffd9fd3b0c6b5815cdc7d6206b82d051b7808
RoSk0 <roskoliy.kirill@gmail.com>
2012-10-11 19:42:41 +0300

    Пофикшено использование функции t().

--- sites/all/themes/drutubetheme/template.php
+++ sites/all/themes/drutubetheme/template.php
@@ -36,6 +36,6 @@ function drutubetheme_process_region(&$vars) {
  */
 function drutubetheme_preprocess_breadcrumb(&$vars) {
   if(current_path() == 'video'){
-    $vars['breadcrumb'][1] = str_replace('[all items]', t('Поиск видео'), $vars['breadcrumb'][1]);
+    $vars['breadcrumb'][1] = str_replace('[all items]', t('Video search'), $vars['breadcrumb'][1]);
   }
 }
